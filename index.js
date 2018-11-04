e0f6b6de909b538f65cd80a0959edb2a84aa2a9f
vidit bhatia <viditbhatia@hotmail.com>
2018-11-03 22:48:23 -0700

    added the component

---
+++ index.js
@@ -0,0 +1,3 @@
+import InstamojoPaymentComponent from "./src/InstaMojoComponent";
+
+export{InstamojoPaymentComponent};
---
+++ src/InstaMojoComponent.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, WebView } from 'react-native';
+import PropTypes from 'prop-types';
+
+InstamojoPaymentComponent.propTypes = {
+  redirect_url: PropTypes.string.isRequired,
+  instamojo_long_url: PropTypes.string.isRequired,
+  callback_on_completion:PropTypes.func
+};
+
+export default class InstamojoPaymentComponent extends React.Component {
+
+  onNavigationChange(webViewState) {
+    let hitUrl = webViewState.url;
+    console.log(hitUrl);
+    if (hitUrl.includes(this.props.redirect_url)) {
+      let payment_final_id = hitUrl.split("request_id=").pop();
+      let return_data = {
+        url: hitUrl,
+        payment_final_id: payment_final_id
+      };
+      if(this.props.callback_on_completion){
+        this.props.callback_on_completion(return_data)
+      }
+    }
+  }
+  
+  render() {
+    
+    return (
+      <WebView
+    ref="webView"
+    source={{ uri: this.props.instamojo_long_url }}
+    onNavigationStateChange={this.onNavigationChange.bind(this)}
+    javaScriptEnabled={true}
+    domStorageEnabled={true}
+    startInLoadingState={true}
+    scalesPageToFit={true}
+    style={{flex:1}}
+    />
+  );
+  }
+}
